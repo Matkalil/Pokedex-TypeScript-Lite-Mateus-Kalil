6856ca97b5b4ac708e5451643a71902c9ff45c4d
Mateus Kuhnen Kalil <matcodekalil@gmail.com>
2026-09-24 18:59:38 -0300

    feat: cria interface POkemonApiResponse (closes #8)

--- src/models/Pokemon.ts
+++ src/models/Pokemon.ts
@@ -1,8 +1,21 @@
-
+// Formato simplificado usado dentro do projeto
 export interface PokemonResumo {
-    id: number;
-    nome: string;
-    tipos:string[];
-    altura: number;
-    peso: number;
+  id: number;
+  nome: string;
+  tipos: string[];
+  altura: number;
+  peso: number;
+}
+
+// Formato recebido da PokeAPI 
+export interface PokemonApiResponse {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
 }
